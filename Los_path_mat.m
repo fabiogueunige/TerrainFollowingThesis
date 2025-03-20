fe8eab1a9bb2b376d04af1ef722b8cb43ab3f09d
fabiogueunige <s5004782@studenti.unige.it>
2025-03-20 15:53:26 +0000

    Sistemata la retta e probabile anche la circonfernza

--- Los_path_mat.m
+++ Los_path_mat.m
@@ -5,7 +5,7 @@ clc
 % Equilibrium point and system specification
 m = 30;
 I = 4.14;
-p = 10; % system pole
+p = 10; % system pole (doveva essere negativo)
 wn = 0.2;
 damp = 0.6;
 
@@ -24,7 +24,7 @@ tau_a = [-25; -2.325; -8.690; -19.4311]; % kl_dot
 tau_r = [-0.2; -55.117; -4.14; 33.6804]; % kl
 
 % Hydrodynamic forces
-tau_d = [-19.5; -147.9; -6.23; -89.4105]; % kl_modl (era -89...
+tau_d = [-19.5; -147.9; -6.23; -89.4105]; % kl_modl
 
 % Virtual mass
 mv = [m; m; I; m] - tau_a;
@@ -37,13 +37,21 @@ dv = -tau_r - 2*tau_d .* abs(speed0);
 Kd_surge = 0;
 Kp_surge = 2*damp*wn*mv(1) - dv(1); % 17.3
 Ki_surge = wn^2 * mv(1); % 55.4
+ti_surge = Kp_surge / Ki_surge;
+Kt_surge = 1 / ti_surge;
 
 % YAW
 Kd_yaw = (p + 2*damp*wn)*mv(3) - dv(3); % 49.34
 Kp_yaw = mv(3)*((wn^2) + 2*damp*p*wn); % 
 Ki_yaw = p*(wn^2)*mv(3);
+ti_yaw = Kp_yaw / Ki_yaw;
+td_yaw = Kd_yaw / Kp_yaw;
+Kt_yaw = 1 / sqrt(ti_yaw*td_yaw);
 
 % DEPTH (pid)
 Kd_dep = (p + 2*damp*wn)*mv(4) + dv(4);
 Kp_dep = mv(4)*((wn^2) + 2*damp*p*wn);
 Ki_dep = p*(wn^2)*mv(4);
+ti_dep = Kp_dep / Ki_dep;
+td_dep = Kd_dep / Kp_dep;
+Kt_dep = 1 / sqrt(ti_dep*td_dep);
