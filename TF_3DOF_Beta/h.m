b754eaa338bd852dfea81c2993405c87a88b8afd
fabiogueunige <s5004782@studenti.unige.it>
2025-07-21 13:28:37 +0100

    boh

--- TF_3DOF_Beta/h.m
+++
@@ -1,9 +0,0 @@
-function z = h(x, gamma, lambda, theta)
-    % Funzione di osservazione: z_k = h(x_k, u_k)
-    delta = [(gamma - (x(2) - theta));
-             (lambda - (x(2) - theta))];
-    
-    z = [x(1)/(cos(delta(1)));
-         x(1)/(cos(delta(2)))];
-    fprintf('Otput predetto (h) y1: %.2f m | y2: %.2f\n', z(1), z(2));
-end
